0b73682eb8a7ee0e481a961ac060de3ca82166d6
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-31 23:25:26 -0700

    Added jquery, popperjs, bootstrap, fontawesome and my_script

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,10 @@
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
+import "jquery"
+import "jquery_ujs"
+import "@popperjs/core"
+import "bootstrap"
+import "@fortawesome/fontawesome-free"
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "./my_script"
